a60ccfc1256de4a08a75223c6a0418a125c0a659
Johnnatan <romerozapatajonathan@gmail.com>
2023-10-31 21:28:45 -0400

    Project init

---
+++ src/main/java/com/example/SpringFrameworkDemo1/SpringFrameworkDemo1Application.java
@@ -0,0 +1,13 @@
+package com.example.SpringFrameworkDemo1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFrameworkDemo1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFrameworkDemo1Application.class, args);
+	}
+
+}
